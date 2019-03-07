deeeec9ee5dba02e4ecab577c86587164c977cdc
Ferris Boran <ferris.boran@gmail.com>
2019-03-07 20:27:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(tellher)
 case tellher
-when tellher.upcase
-  return "NO, NOT SINCE 1938!"
 when "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+when tellher.upcase
+  return "NO, NOT SINCE 1938!"
 else
   return "HUH?! SPEAK UP, SONNY!"
 end
